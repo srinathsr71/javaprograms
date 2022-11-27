d6632dbbfd6a9c158d51dd1a20127ce6c637fac5
srinathsr71 <srinathsr71@gmail.com>
2022-11-27 14:14:44 +0530

    commit demo

---
+++ demo.java
@@ -0,0 +1,81 @@
+public class demo{
+
+public static void main(String args[]){
+
+
+System.out.println("string is"+2+10);//string is 12 //concatinating
+System.out.println(2+10+"string is");//12 string is//after performing
+operation the two will concatinating.
+
+}
+
+}
+
+string is210
+12string is
+
+
+"string is srinath"+2+10=
+
+2+10+3+13+"srinath"
+
+
+
+
+A thermometer measures temperature in whole degrees only. If the temperature
+falls below 18 degrees, the heating is switched off. It is switched on again when the
+temperature reaches 21 degrees. What are the best values in degrees to cover all
+equivalence partitions?
+A. 15, 19 and 25.
+B. 17, 18 and19.
+C. 18, 20 and22.
+D. 16, 26 and 32.
+
+
+A wholesaler sells printer cartridges. The minimum order quantity is 5. There is
+a 20% discount for orders of 100 or more printer cartridges. You have been asked to
+prepare test cases using various values for the number of printer cartridges ordered.
+Which of the following groups contain three test inputs that would be generated using
+Boundary Value Analysis?
+A. 5, 6, 20
+B. 4, 5, 80
+C. 4, 5, 99
+D. 1, 20, 100
+
+
+bva:-min-1,min,min+1
+	,max-1,max,max+1
+
+4,5,6
+99,100,101.
+
+4,5,99
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
